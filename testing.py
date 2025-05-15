878628a882868632f5c122e90e91d944bf6f3a28
William-67 <lwei075@uottawa.ca>
2025-05-14 21:31:57 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 # genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-genai.configure(api_key="AIzaSyBKsQIDhRIPVgW_hdteVxhOAHFL2G7Gi-Y")
+genai.configure(api_key="apikey")
 
 for model in genai.list_models():
     print(f"{model.name} - {model.supported_generation_methods}")
